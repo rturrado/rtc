4c7359fabf9bcb3d755742d015f02bef69276fc8
rturrado <68099809+rturrado@users.noreply.github.com>
2022-04-27 19:49:41 +0200

    - Changed ifndef/define/endif to pragma once for print.h. - Added some comments and did some minor aesthetic changes to print.h. - Updated version to 10.5.

--- include/rtc/print.h
+++ include/rtc/print.h
@@ -1,70 +1,75 @@
-#ifndef RTC_PRINT_H
-#define RTC_PRINT_H
+#pragma once
 
 #include <algorithm>  // for_each, sort
 #include <concepts>
 #include <iomanip>  // setfill, setw
-#include <ios>  // hex
+#include <ios>  // dec, hex
 #include <ostream>
 #include <utility>  // pair
 
 
-namespace rtc::print
-{
-    // Print container
+namespace rtc::print {
     template <typename T>
-    concept printable = requires (std::ostream & os, const T & t) { os << t; };
+    concept printable = requires (std::ostream& os, const T& t) {
+        os << t;
+    };
 
     template <typename C>
-    concept implements_operator_extraction = requires (std::ostream & os, const C & c) { os << c; };
+    concept implements_operator_extraction = requires (std::ostream& os, const C& c) {
+        os << c;
+    };
 
+    // Print container
     template <typename C>
         requires (!std::same_as<typename C::value_type, uint8_t>)
-    && (!std::same_as<typename C::value_type, char>)
+        && (!std::same_as<typename C::value_type, char>)
         && (!std::is_convertible_v<typename C::value_type, std::string_view>)
         && (!implements_operator_extraction<C>)
         && printable<typename C::value_type>
-        std::ostream& operator<<(std::ostream& os, const C& c)
-    {
+    std::ostream& operator<<(std::ostream& os, const C& c) {
         os << "{";
         std::for_each(cbegin(c), cend(c), [first = true, &os](const auto& n) mutable {
-            os << (first ? " " : ", ") << n; first = false;
+            os << (first ? " " : ", ") << n;
+            first = false;
         });
         os << " }";
         return os;
     };
 
+    // Print container of uint8_t
     template <typename C>
         requires std::same_as<typename C::value_type, uint8_t> && (!implements_operator_extraction<C>)
-    std::ostream& operator<<(std::ostream& os, const C& c)
-    {
+    std::ostream& operator<<(std::ostream& os, const C& c) {
         os << "{";
         std::for_each(cbegin(c), cend(c), [first = true, &os](const auto& n) mutable {
-            os << (first ? " " : ", ") << "0x" << std::hex << std::setw(2) << std::setfill('0') << static_cast<uint16_t>(n); first = false;
+            os << (first ? " " : ", ") << "0x" << std::hex << std::setw(2) << std::setfill('0') << static_cast<uint16_t>(n) << std::dec;
+            first = false;
         });
         os << " }";
         return os;
     };
 
+    // Print container of chars
     template <typename C>
         requires std::same_as<typename C::value_type, char> && (!implements_operator_extraction<C>)
-    std::ostream& operator<<(std::ostream& os, const C& c)
-    {
+    std::ostream& operator<<(std::ostream& os, const C& c) {
         os << "{";
         std::for_each(cbegin(c), cend(c), [first = true, &os](const auto& n) mutable {
-            os << (first ? " " : ", ") << "'" << n << "'"; first = false;
+            os << (first ? " " : ", ") << "'" << n << "'";
+            first = false;
         });
         os << " }";
         return os;
     };
 
+    // Print container of strings
     template <typename C>
         requires std::is_convertible_v<typename C::value_type, std::string_view> && (!implements_operator_extraction<C>)
-    std::ostream& operator<<(std::ostream& os, const C& c)
-    {
+    std::ostream& operator<<(std::ostream& os, const C& c) {
         os << "{";
         std::for_each(cbegin(c), cend(c), [first = true, &os](const auto& n) mutable {
-            os << (first ? " " : ", ") << "\"" << n << "\""; first = false;
+            os << (first ? " " : ", ") << "\"" << n << "\"";
+            first = false;
         });
         os << " }";
         return os;
@@ -73,12 +78,7 @@ namespace rtc::print
 
     // Print pair
     template<typename T, typename U = T>
-    std::ostream& operator<<(std::ostream& os, const std::pair<T, U>& p)
-    {
+    std::ostream& operator<<(std::ostream& os, const std::pair<T, U>& p) {
         return os << "[" << p.first << ", " << p.second << "]";
     }
-
 }  // namespace rtc::print
-
-
-#endif  // RTC_PRINT_H
