dea484a42b45593d6a72de33c0e89ea7c79f8047
rturrado <68099809+rturrado@users.noreply.github.com>
2022-05-04 01:02:29 +0200

    - Added fmt::formatter for rtc::pretty_print::indentation. - Added tests for rtc::pretty_print::indentation. - Updated version to 1.0.6.

--- include/rtc/pretty_print.h
+++ include/rtc/pretty_print.h
@@ -1,19 +1,24 @@
 #pragma once
 
-#include <algorithm>  // fill_n
+#include <fmt/format.h>
+#include <fmt/ostream.h>
+#include <fmt/ranges.h>
 #include <iterator>  // ostream_iterator
 #include <ostream>
+#include <range/v3/view/repeat_n.hpp>
 #include <string>
 
 
 namespace rtc::pretty_print {
+    const auto default_indentation_value{ "    " };
+
     struct indentation {
         size_t level{};
-        std::string value{ "    " };
+        std::string value{ default_indentation_value };
     };
 
     inline std::ostream& operator<<(std::ostream& os, const indentation& i) {
-        std::fill_n(std::ostream_iterator<std::string>(os), i.level, i.value);
+        fmt::print(os, "{}", i);
         return os;
     }
 
@@ -24,3 +29,17 @@ namespace rtc::pretty_print {
         return i + increase;
     }
 }  // namespace rtc::pretty_print
+
+
+template<>
+struct fmt::formatter<rtc::pretty_print::indentation> {
+    template<typename ParseContext>
+    constexpr auto parse(ParseContext& ctx) {
+        return ctx.begin();
+    }
+
+    template <typename FormatContext>
+    auto format(const rtc::pretty_print::indentation& i, FormatContext& ctx) const -> decltype(ctx.out()) {
+        return fmt::format_to(ctx.out(), "{}", fmt::join(ranges::views::repeat_n(i.value, i.level), ""));
+    }
+};
--- test/pretty_print.cpp
+++ test/pretty_print.cpp
@@ -1,4 +1,43 @@
 #include "rtc/pretty_print.h"
 
+#include <fmt/format.h>
+#include <fmt/ostream.h>
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include <sstream>  // ostringstream
+
+using namespace rtc::pretty_print;
+
+
+TEST(operator_extraction, default_indentation) {
+    std::ostringstream oss{};
+    oss << indentation{} << 25 << "\n";
+    EXPECT_THAT(oss.str(), ::testing::HasSubstr(fmt::format("{}{}{}", "", 25, "\n")));
+}
+TEST(operator_extraction, custom_indentation) {
+    std::ostringstream oss{};
+    oss << indentation{1, "***"} << 25 << "\n";
+    EXPECT_THAT(oss.str(), ::testing::HasSubstr(fmt::format("{}{}{}", "***", 25, "\n")));
+}
+
+TEST(fmt_print, default_indentation) {
+    std::ostringstream oss{};
+    fmt::print(oss, "{}{}{}", indentation{}, 25, "\n");
+    EXPECT_THAT(oss.str(), ::testing::HasSubstr(fmt::format("{}{}{}", "", 25, "\n")));
+}
+TEST(fmt_print, custom_indentation) {
+    std::ostringstream oss{};
+    fmt::print(oss, "{}{}{}", indentation{1, "***"}, 25, "\n");
+    EXPECT_THAT(oss.str(), ::testing::HasSubstr(fmt::format("{}{}{}", "***", 25, "\n")));
+}
+
+TEST(indentation_plus_increase, n_1) {
+    std::ostringstream oss{};
+    fmt::print(oss, "{}{}{}", indentation{} + 1, 25, "\n");
+    EXPECT_THAT(oss.str(), ::testing::HasSubstr(fmt::format("{}{}{}", default_indentation_value, 25, "\n")));
+}
+TEST(increase_plus_indentation, n_1) {
+    std::ostringstream oss{};
+    fmt::print(oss, "{}{}{}", 1 + indentation{}, 25, "\n");
+    EXPECT_THAT(oss.str(), ::testing::HasSubstr(fmt::format("{}{}{}", default_indentation_value, 25, "\n")));
+}
