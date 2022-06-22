4bddc1c4e055ecfe52de840a96a9d141b342c5ae
rturrado <68099809+rturrado@users.noreply.github.com>
2022-06-22 20:05:05 +0200

    Added invalid_month_error.

--- include/rtc/chrono.h
+++ include/rtc/chrono.h
@@ -10,4 +10,8 @@ namespace rtc::chrono {
     struct invalid_date_error : public std::runtime_error {
         invalid_date_error(const std::chrono::year_month_day& ymd) : std::runtime_error{ std::format("{}", ymd) } {}
     };
+
+    struct invalid_month_error : public std::runtime_error {
+        invalid_month_error(const std::chrono::month& m) : std::runtime_error{ std::format("{}", m) } {}
+    };
 }  // namespace rtc::chrono
--- test/chrono.cpp
+++ test/chrono.cpp
@@ -6,6 +6,7 @@
 #include <chrono>
 #include <string>
 
+namespace ch = std::chrono;
 using namespace rtc::chrono;
 using namespace std::chrono_literals;
 
@@ -14,5 +15,12 @@ TEST(invalid_date_error, constructor) {
     EXPECT_THAT(
         std::string{ invalid_date_error{ 2022y / 20 / 50 }.what() },
         ::testing::ContainsRegex("2022.20.50 is not a valid date")
+    );
+}
+
+TEST(invalid_month_error, constructor) {
+    EXPECT_THAT(
+        std::string{ invalid_month_error{ ch::month{ 20 } }.what() },
+        ::testing::HasSubstr("20 is not a valid month")
     );
 }
