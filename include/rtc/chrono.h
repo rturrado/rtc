d2e1743468ebba82fcddb28e660d809a2d51d3d2
rturrado <68099809+rturrado@users.noreply.github.com>
2022-09-05 19:15:23 +0200

    Fixing chrono.h to work with clang/gcc. Updated CMakeLists.txt and CMakePresets.json.

--- include/rtc/chrono.h
+++ include/rtc/chrono.h
@@ -1,18 +1,27 @@
 #pragma once
 
-#include <chrono>
 #include <date/date.h>
-#include <format>
+#include <fmt/format.h>
 #include <stdexcept>  // runtime_error
 #include <string>
 
 
 namespace rtc::chrono {
     struct invalid_date_error : public std::runtime_error {
-        invalid_date_error(const date::year_month_day& ymd) : std::runtime_error{ std::format("{}", ymd) } {}
+        invalid_date_error(const date::year_month_day& ymd) : std::runtime_error{ "" } {
+            message_ = fmt::format(
+                "{:04}-{:02}-{:02} is not a valid date",
+                int(ymd.year()), unsigned(ymd.month()), unsigned(ymd.day())
+            );
+        }
+        const char* what() const noexcept { return message_.c_str(); }
+    private:
+        std::string message_{};
     };
 
     struct invalid_month_error : public std::runtime_error {
-        invalid_month_error(const date::month& m) : std::runtime_error{ std::format("{}", m) } {}
+        invalid_month_error(const date::month& m)
+            : std::runtime_error{ fmt::format("{} is not a valid month", unsigned(m)) }
+        {}
     };
 }  // namespace rtc::chrono
